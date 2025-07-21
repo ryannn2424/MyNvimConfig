df995e6d13ea3eb583e4b6bda51354dd4605ebe8
Ryan Ray <ryanray25@hotmail.com>
2025-07-21 00:13:50 -0700

    Added bufferline plugin

---
+++ lua/ryanray/plugins/bufferline.lua
@@ -0,0 +1,14 @@
+return {
+  'akinsho/bufferline.nvim',
+  config = function()
+    require("bufferline").setup {
+      options = {
+        indicator = {
+          style = 'underline'
+        },
+        separator_style = "slant"
+      }
+    }
+  end,
+  vim.keymap.set('n', '<M-1>', 'lua require("bufferline").go_to(1, true)<cr>', {noremap = true})
+}
